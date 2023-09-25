837f1873ae0a64035e1a3a73cf5678db83be907d
Umar Ibnu Zainal Muttaqin <marinska354@gmail.com>
2023-09-25 18:19:36 +0700

    update

--- app.js
+++ app.js
@@ -23,7 +23,32 @@ client.on('messageCreate', (msg) => {
         // if (msg.content == '.xx'){
     //     msg.channel.send('x');
     //     return;
-    // }
+    // } 
+
+    if (msg.content == '.pagi'){
+        msg.channel.send('https://cdn.discordapp.com/attachments/723550821832458281/1155824881229897789/stlnppa.png');
+        return;
+    }
+
+        if (msg.content == '.ok'){
+        msg.channel.send('https://i.pinimg.com/originals/54/a4/00/54a4008daad4565a9b5db1b94e59c74c.jpg');
+        return;
+    } 
+
+    if (msg.content == '.ada' || msg.content == '.adasaja'){
+        msg.channel.send('https://cdn.discordapp.com/attachments/812956551320043523/1064502375160762379/adaadasaja.jpg');
+        return;
+    }
+
+    if (msg.content == '.tergantung'){
+        msg.channel.send('https://cdn.discordapp.com/attachments/812956551320043523/1057358476877250601/ea.jpg');
+        return;
+    }
+    
+    if (msg.content == '.ampun'){
+        msg.channel.send('https://cdn.discordapp.com/attachments/812956551320043523/813066861365166090/jaog.jpg');
+        return;
+    }
 
     if (msg.content == '.bacot' || msg.content == '.bct'){
         msg.channel.send('https://cdn.discordapp.com/attachments/723550821832458281/1155816673220247552/image.png');
@@ -101,22 +126,7 @@ client.on('messageCreate', (msg) => {
     }
 
     if (msg.content == '.cmdlist'){
-        msg.channel.send(`Command list-nya ini masbree 
-        \n.asik
-        \n.renjon / .renzon
-        \n.telat
-        \n.wleo
-        \n.maulima
-        \n.berak
-        \n.nyimak
-        \n.diam
-        \n.bacot / .bct
-        \n.pergi
-        \n.lucu
-        \n.waduh
-        \n.matilampu
-        \n.malas
-        \n.apasi / .apasih`);
+        msg.channel.send('Command list-nya ini masbree\n`.pagi`\n`.ok`\n`.ada`\n`.tergantung`\n`.ampun `\n`.asik`\n`.renjon / .renzon`\n`.telat`\n`.wleo`\n`.maulima`\n`.berak`\n`.nyimak`\n`.diam`\n`.bacot / .bct`\n`.pergi`\n`.lucu`\n`.waduh`\n`.matilampu`\n`.malas`\n`.apasi / .apasih`');
         return;
     }
 });
